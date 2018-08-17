bed09035fff4548d9ab8966b762d22fab00829c3
Vasco-Pixel <41165853+Vasco-Pixel@users.noreply.github.com>
2018-08-17 17:18:09 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -114,16 +114,16 @@ window.addEventListener('load', function () {
     var div = document.createElement('div');
     div.setAttribute('class', 'post block bc2');
     div.innerHTML = '<div id="minimapbg" style="position: absolute; right: 1em; bottom: 1em; z-index: 2;">' +
-        '<div class="posy" id="posyt" style="background-size: 100%; background-image: url(https://cdn.pbrd.co/images/HqxSkRb.png); color: rgb(255, 255, 255); text-align: center; line-height: 42px; vertical-align: middle; width: auto; height: auto; border-radius: 21px; padding: 6px;">' +
+        '<div class="posy" id="posyt" style="background-size: 100%; background-image: url(https://image.ibb.co/g2POxe/vbg.png); color: rgb(255, 255, 255); text-align: center; line-height: 42px; vertical-align: middle; width: auto; height: auto; border-radius: 21px; padding: 6px;">' +
         '<div id="minimap-text" style="display: none;"></div>' +
         '<div id="minimap-box" style="position: relative;width:420px;height:300px">' +
         '<canvas id="minimap" style="width: 100%; height: 100%;z-index:1;position:absolute;top:0;left:0;"></canvas>' +
         '<canvas id="minimap-board" style="width: 100%; height: 100%;z-index:2;position:absolute;top:0;left:0;"></canvas>' +
         '<canvas id="minimap-cursor" style="width: 100%; height: 100%;z-index:3;position:absolute;top:0;left:0;"></canvas>' +
         '</div><div id="minimap-config" style="line-height:20px;">' +
-		'<a href=https://discord.io/italianempire target="_blank">Discord' +
-        '</a> | <span id="hide-map" style="cursor:pointer;color:white"> Oscurare' +
-        '</span> | <span id="follow-mouse" style="cursor:pointer;">Seguire il mouse' +
+		'<a href=https://discord.io/vascop target="_blank">Discord' +
+        '</a> | <span id="hide-map" style="cursor:pointer;color:white"> Minimizar' +
+        '</span> | <span id="follow-mouse" style="cursor:pointer;">Seguir o mouse' +
         '</span>| Zoom: <span id="zoom-plus" style="cursor:pointer;font-weight:bold;">+</span> / ' +
         '<span id="zoom-minus" style="cursor:pointer;font-weight:bold;">-</span>' +
         '</div>' +
